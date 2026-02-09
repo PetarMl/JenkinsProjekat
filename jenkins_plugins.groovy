1afcf3acb41cea607bb81586ac8f1787213e6e8e
PetarMl <petar.mladenov@pmf.edu.rs>
2026-02-10 00:52:52 +0100

    Test1

---
+++ jenkins_plugins.groovy
@@ -0,0 +1,13 @@
+import jenkins.model.Jenkins
+
+pm=Jenkins.instance.pluginManager
+uc=Jenkins.instance.UpdateCenter
+
+uc.updateAllSites()
+["github","docker-workflow","prometheus","ansible"].each{
+    if(![pm.getPlugin(it)]){
+        deployment=uc.getPlugin(it).deploy(true)
+        deployment.get()
+    }
+}
+Jenkins.instance.restart()
